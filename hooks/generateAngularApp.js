e5fb540129a27e0931b1d26e20e1fcbc7459f7a1
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-10-03 13:36:40 -0300

    fix: change angular app generation to shell command

--- hooks/generateAngularApp.js
+++ hooks/generateAngularApp.js
@@ -1,5 +1,19 @@
-const angular = require('@angular/cli');
-const run = angular.default;
+/**
+ * Executes a shell command and return it as a Promise.
+ * @param cmd {string}
+ * @return {Promise<string>}
+ */
+function execShellCommand(cmd) {
+    const exec = require('child_process').exec;
+    return new Promise((resolve, reject) => {
+     exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+       console.warn(error);
+      }
+      resolve(stdout? stdout : stderr);
+     });
+    });
+}
 
 module.exports = {
     'generate:before': async (generator) => {
@@ -9,6 +23,9 @@ module.exports = {
                 cliArgs: [
                     'new',
                     '--force',
+                    '--defaults',
+                    '--interactive',
+                    'false',
                     angularAppName,
                     '--skip-install',
                     '--skip-tests',
@@ -20,10 +37,10 @@ module.exports = {
             };
             process.chdir(generator.targetDir);
             try {
-                const result = await run(options);
-                console.log('angular app successfully generated, code: ' + result);
+                const result = await execShellCommand(`ng ${options.cliArgs.join(" ")}`);
+                console.log('Angular app generated successfully');
                 generator.targetDir = `${generator.targetDir}/${angularAppName}/src/app`;
-            } catch {
+            } catch(e) {
                 console.log('some error has occurred while generating angular code');
             }
             process.chdir(process.env.PWD);
